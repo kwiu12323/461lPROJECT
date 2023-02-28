3769ddcde2f968e75241d1da83529d354e00e5d4
dcl2239 <48533861+dcl2239@users.noreply.github.com>
2023-02-28 00:45:10 -0600

    Add files via upload

---
+++ Frontend/src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Userid() {
+  return (
+    <div>
+        Login
+        UserName
+        <input type= "text" />
+        PassWord
+        <input type= "text" />
+        
+    </div>
+  )
+}
---
+++ Frontend/src/NewProject.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function NewProject() {
+  return (
+    <div>NewProject
+        Project Name
+        <input type= "text" />
+        Description
+        <input type= "text" />
+        Project id
+        <input type= "text" />
+    </div>
+  )
+}
